091d66cc7256fc79aaa612471cf7516910f9f6d1
Antonio Marcos da Silva <antonio.ms25@aluno.ifsc.edu.br>
2025-04-03 21:40:47 -0300

    Aula 05 - Commit inicial

---
+++ aula-05/src/main/java/ads/poo/App.java
@@ -0,0 +1,13 @@
+package ads.poo; //Usar sempre o domínio da organização em ordem inversa
+
+public class App {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+
+        while (i < j) {
+            System.out.println(i);
+            i = i + 1;
+        }
+    }
+}
