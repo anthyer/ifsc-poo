46bb57054093165e9349d0f806df1c9aef5c9769
Antonio Marcos da Silva <antonio.ms25@aluno.ifsc.edu.br>
2025-04-17 22:19:06 -0300

    Exercicio Caneta Concluído

--- aula-07/app/src/main/java/ads/poo/Caneta.java
+++ aula-07/app/src/main/java/ads/poo/Caneta.java
@@ -6,24 +6,57 @@ public class Caneta {
     private double nivelTinta;
     private boolean aberta;
 
-    // Métodos
+    // Construtor
+    public Caneta(String c, double n, boolean a) {
+        this.cor = c;
+        this.nivelTinta = n;
+        this.aberta = a;
+    }
+
+    // Getters e Setters
     public void definirCor(String c) {
-        cor = c;
+        this.cor = c;
     }
 
     public void definirNivelTinta(double n) {
-        nivelTinta = n;
+        this.nivelTinta = (n < 0 || n > 100) ? 100 : n;
     }
 
-    public void trocarEstado() {
-        aberta = !aberta;
+    public void definirAberto(boolean a) {
+        this.aberta = a;
+    }
+
+    public String isCor() {
+        return cor;
+    }
+
+    public double isNivelTinta() {
+        return nivelTinta;
     }
 
     public boolean isAberta() {
         return aberta;
     }
 
-    public String isCor() {
-        return cor;
+    // Métodos
+    public void trocarEstado() {
+        aberta = !aberta;
+    }
+
+    public String desenhar(int n) {
+        if (aberta && n > 0) {
+            StringBuilder aux = new StringBuilder("");
+            for (int i = 0; i < n; i++) {
+                if (nivelTinta > 9) {
+                    aux.append("_ ");
+                    nivelTinta = nivelTinta - 10;
+                } else {
+                    return aux.toString();
+                }
+            }
+            return aux.toString();
+        } else {
+            return "";
+        }
     }
 }
