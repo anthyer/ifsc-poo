7cab82d64b8a1a7e1cd836cd0798b99be8b51db0
Antonio Marcos da Silva <antonio.ms25@aluno.ifsc.edu.br>
2025-05-12 22:29:40 -0300

    Aula 9 - Atividade concluída.

---
+++ aula-09/app/src/main/java/ads/poo/Pessoa.java
@@ -0,0 +1,34 @@
+package ads.poo;
+
+public class Pessoa {
+    private String nome;
+    private String email;
+
+    public Pessoa(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Nome: ");
+        sb.append(nome).append(", Email: ").append(email);
+        return sb.toString();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
